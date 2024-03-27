0e5ae809aff1667e12a5696b89f7dc25df60e09c
Nikesh Chaudhary <nikeshchaudhary52494@gmail.com>
2024-03-28 00:06:07 +0530

    frontend setup done

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from "react-dom/client";
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from './store/store.js';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </Router>
+    </Provider>
   </React.StrictMode>,
-)
+);
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,10 @@
+function Login() {
+
+    return (
+        <>
+            <p className="bg-red-500">Hello from frontend</p>
+        </>
+    )
+}
+
+export default Login
