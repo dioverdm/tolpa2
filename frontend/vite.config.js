0553e3b2fdc9bce44d9b2176d0e2908dcc9bc7c0
Nikesh Chaudhary <nikeshchaudhary52494@gmail.com>
2024-03-30 15:48:09 +0530

    vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      "/api": {
-        target: "https://buzzhive-4zkh.onrender.com",
-        credentials: true,
+export default ({ mode }) => {
+  const isProduction = mode === 'production';
+
+  const backendURL = isProduction
+    ? 'https://buzzhive-4zkh.onrender.com' // Production backend URL
+    : 'http://localhost:5000'; // Development backend URL
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      port: 3000,
+      proxy: {
+        "/api": {
+          target: backendURL,
+          changeOrigin: true,
+          credentials: true,
+        },
       },
     },
-  },
-});
+  });
+};
